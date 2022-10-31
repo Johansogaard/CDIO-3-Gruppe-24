adee59b2db8a8e6aa2aa5ce0b54ef6f2ccf24513
Johan Jorgensen <Johansogaardjorgensen@gmail.com>
2022-10-31 19:28:47 +0100

    lavet en metode der læser fra fil

---
+++ GameController.java
@@ -0,0 +1,40 @@
+import java.io.*;
+
+public class GameController {
+    final static String file = "GameText.txt";
+
+    public static void main(String[] args) {
+        System.out.println(readTextFromFile(file,"jailMessage"));
+
+    }
+
+    public static String readTextFromFile(String file, String stringName)
+    {
+        String message;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+
+            String currentLine = reader.readLine();
+            //"Loop begin indtil der ikke er mere at læse"
+           // læs én linie.
+            // "Loop end
+            while (true) {
+               if (currentLine.equals( stringName))
+                {
+                    currentLine = reader.readLine();
+                    break;
+                }
+                currentLine = reader.readLine();
+            }
+            message = currentLine;
+            reader.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return message;
+
+
+    }
+}
