f6da31ac5e5d3ce724009b0f0e19e81c86a3f47d
Anders Jefsen <andersjeff397@gmail.com>
2022-11-15 15:08:00 +0100

    TerningerTest

---
+++ test/spil/PlayerTest.java
@@ -0,0 +1,68 @@
+package spil;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Test
+    public void getPos() {
+    }
+
+    @Test
+    public void setPos() {
+    }
+
+    @Test
+    public void isJail() {
+    }
+
+    @Test
+    public void setJail() {
+    }
+
+    @Test
+    public void tilføjspillerGui() {
+    }
+
+    @Test
+    public void getKonto() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void spil() {
+    }
+
+    @Test
+    public void turn() {
+    }
+
+    @Test
+    public void setCar() {
+    }
+
+    @Test
+    public void displayCard() {
+    }
+
+    @Test
+    public void payRent() {
+    }
+
+    @Test
+    public void getRent() {
+    }
+
+    @Test
+    public void buyField() {
+    }
+
+    @Test
+    public void injail() {
+    }
+}
